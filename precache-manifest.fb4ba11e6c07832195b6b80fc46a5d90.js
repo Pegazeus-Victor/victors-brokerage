42867845c4bc3aa3b434229749a5b2c860b43130
Pegazeus-Victor <61429368+Pegazeus-Victor@users.noreply.github.com>
2020-05-13 11:12:41 -0700

    Updates

---
+++ precache-manifest.fb4ba11e6c07832195b6b80fc46a5d90.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "643032c3b9c9544d9216364ff4278723",
+    "url": "/victors-brokerage/index.html"
+  },
+  {
+    "revision": "ad15866ecde738d5f6bb",
+    "url": "/victors-brokerage/static/css/10.37c8f0ee.chunk.css"
+  },
+  {
+    "revision": "e08dfb7956135635c103",
+    "url": "/victors-brokerage/static/css/11.1d9a2034.chunk.css"
+  },
+  {
+    "revision": "db2092b849e4af1ac338",
+    "url": "/victors-brokerage/static/css/12.1d9a2034.chunk.css"
+  },
+  {
+    "revision": "6580255a521b1e0fa83c",
+    "url": "/victors-brokerage/static/css/2.21d3525d.chunk.css"
+  },
+  {
+    "revision": "9aebd1b971c4525d21f0",
+    "url": "/victors-brokerage/static/css/6.93e000bb.chunk.css"
+  },
+  {
+    "revision": "0f83aedfa0192502a55c",
+    "url": "/victors-brokerage/static/css/7.37c8f0ee.chunk.css"
+  },
+  {
+    "revision": "41d497e7188fc86a32f3",
+    "url": "/victors-brokerage/static/css/8.37c8f0ee.chunk.css"
+  },
+  {
+    "revision": "675186887821324a799e",
+    "url": "/victors-brokerage/static/css/9.37c8f0ee.chunk.css"
+  },
+  {
+    "revision": "c19b3991420825e7359e",
+    "url": "/victors-brokerage/static/css/main.8390ce8f.chunk.css"
+  },
+  {
+    "revision": "94471ccb6276ebbe59e3",
+    "url": "/victors-brokerage/static/js/0.ea93546e.chunk.js"
+  },
+  {
+    "revision": "5051e70ede3031d939a4",
+    "url": "/victors-brokerage/static/js/1.7d1caf62.chunk.js"
+  },
+  {
+    "revision": "ad15866ecde738d5f6bb",
+    "url": "/victors-brokerage/static/js/10.95d3bdeb.chunk.js"
+  },
+  {
+    "revision": "e08dfb7956135635c103",
+    "url": "/victors-brokerage/static/js/11.2047bd28.chunk.js"
+  },
+  {
+    "revision": "db2092b849e4af1ac338",
+    "url": "/victors-brokerage/static/js/12.2948f801.chunk.js"
+  },
+  {
+    "revision": "e4f7111fbaed187627f7",
+    "url": "/victors-brokerage/static/js/13.fecb4174.chunk.js"
+  },
+  {
+    "revision": "2bb918d782a4d14fd7ff",
+    "url": "/victors-brokerage/static/js/14.8523b573.chunk.js"
+  },
+  {
+    "revision": "6580255a521b1e0fa83c",
+    "url": "/victors-brokerage/static/js/2.2b57acaf.chunk.js"
+  },
+  {
+    "revision": "000d6c36287224a7ff48f88e64636454",
+    "url": "/victors-brokerage/static/js/2.2b57acaf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d0dd0a78754f70dd93c7",
+    "url": "/victors-brokerage/static/js/3.60bc2df6.chunk.js"
+  },
+  {
+    "revision": "362cd232e6b400756e3bc1723069a8cd",
+    "url": "/victors-brokerage/static/js/3.60bc2df6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9aebd1b971c4525d21f0",
+    "url": "/victors-brokerage/static/js/6.680bf81f.chunk.js"
+  },
+  {
+    "revision": "8e01edd47da9aa824272b7ab763bdf7f",
+    "url": "/victors-brokerage/static/js/6.680bf81f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0f83aedfa0192502a55c",
+    "url": "/victors-brokerage/static/js/7.1d915a3c.chunk.js"
+  },
+  {
+    "revision": "41d497e7188fc86a32f3",
+    "url": "/victors-brokerage/static/js/8.6ed08aa5.chunk.js"
+  },
+  {
+    "revision": "675186887821324a799e",
+    "url": "/victors-brokerage/static/js/9.7868dec1.chunk.js"
+  },
+  {
+    "revision": "c19b3991420825e7359e",
+    "url": "/victors-brokerage/static/js/main.49f95ea5.chunk.js"
+  },
+  {
+    "revision": "699a8e8eb9729877a932",
+    "url": "/victors-brokerage/static/js/runtime-main.dcc57d2b.js"
+  },
+  {
+    "revision": "8c20481d522a1bf8c654d2fcb5d769b2",
+    "url": "/victors-brokerage/static/media/bribe.8c20481d.svg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/victors-brokerage/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/victors-brokerage/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/victors-brokerage/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/victors-brokerage/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/victors-brokerage/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
